bc6c0334bc64af7ceb53fd6be1a29ce52ceeba96
Gerzhan <nikolay.gerzhan@gmail.com>
2019-02-15 07:47:12 +0700

    Add Sidebar

--- apps/tsj/admin/src/app/app.module.ts
+++ apps/tsj/admin/src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbSidebarService
+} from '@nebular/theme';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,9 +20,10 @@ import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'corporate' }),
-    NbLayoutModule
+    NbLayoutModule,
+    NbSidebarModule
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
