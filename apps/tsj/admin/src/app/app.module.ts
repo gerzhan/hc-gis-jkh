2339fca1d9f57eab8145be97c02d49a4f68c1f88
Gerzhan <nikolay.gerzhan@gmail.com>
2019-02-15 07:41:21 +0700

    Add Nebular

--- apps/tsj/admin/src/app/app.module.ts
+++ apps/tsj/admin/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
